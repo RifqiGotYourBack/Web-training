fc7f625794771846ceaea2e8cdc4667f8aab1fcb
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-06-09 18:32:28 +0700

    Added tanks and country

---
+++ migrations/20220609111920-create-tank.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Tanks", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      tankName: {
+        type: Sequelize.STRING,
+      },
+      countryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Countries",
+          key: "id",
+        },
+      },
+      turretCaliber: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Tanks");
+  },
+};
